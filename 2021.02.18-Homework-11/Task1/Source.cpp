f25247ac9f074f1e62f474a5c0c4c0bce101e18d
cherenkow <CherenkowS@yandex.ru>
2021-02-27 13:43:08 +0300

    some fixes

--- 2021.02.18-Homework-11/Task1/Source.cpp
+++ 2021.02.18-Homework-11/Task1/Source.cpp
@@ -72,7 +72,7 @@ int binaryInt(int s) {
 	}
 	return k;
 }
-void binaryprInt(int s, int t = 0, int n = 23) {
+void binaryprInt(int s,int bits,int t = 0, int n = 23) {
 	int k = 0;
 	int* arr = new int[n] { 0 };
 	while (s >= 2) {
@@ -83,47 +83,60 @@ void binaryprInt(int s, int t = 0, int n = 23) {
 	arr[k] = s % 2;
 	(t == 0 ? k += 0 : k -= 1);
 	for (int i = k; i >= 0; --i) {
+		if (bits % 8 == 0) {
+			cout << " ";
+		}
 		cout << arr[i];
+		bits += 1;
 	}
 }
-void binaryR(float f, int k, int n = 23) {
+void binaryR(float f,int bits, int k, int n = 23) {
 	float ff = f - (int)f;
 	for (int i = k + 1; i <= n; ++i) {
 		if (ff > 0) {
 			ff *= 2;
+			if (bits % 8 == 0) {
+				cout << " ";
+			}
 			cout << (int)(ff);
 			ff = ff - (int)ff;
+			bits += 1;
 			continue;
 		}
+		if (bits % 8 == 0) {
+			cout << " ";
+		}
 		cout << "0";
+		bits += 1;
+
 	}
 }
 void printBits(double f) {
 	if (f > 0) {
-		cout << "0 ";
+		cout << "0";
 	}
 	else {
-		cout << "1 ";
+		cout << "1";
 		f *= (-1);
 	}
 	int k = binaryInt(int(f));
-	binaryprInt(k + 1023); cout << " ";
-	binaryprInt(int(f), 1, 52);
-	binaryR(f, k, 52);
+	binaryprInt(k + 1023,1,0,52);
+	binaryprInt(int(f),12, 1, 52);
+	binaryR(f,12+k, k, 52);
 }
 
 void printBits(float f) {
 	if (f > 0) {
-		cout << "0 ";
+		cout << "0";
 	}
 	else {
-		cout << "1 ";
+		cout << "1";
 		f *= (-1);
 	}
 	int k = binaryInt(int(f));
-	binaryprInt(k + 127); cout << " ";
-	binaryprInt(int(f), 1);
-	binaryR(f, k);
+	binaryprInt(k + 127,1);
+	binaryprInt(int(f),9, 1);
+	binaryR(f,9+k, k);
 }
 
 void printBits(long double f) {
@@ -155,7 +168,7 @@ int main() {
 	cin >> f;
 	printBits(f);
 	cout << endl;
-	
+
 	double g = 0;
 	cin >> g;
 	printBits(g);
@@ -165,5 +178,6 @@ int main() {
 	cin >> h;
 	printBits(h);
 
+	
 	return 0;
 }
